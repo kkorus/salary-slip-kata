0a7a2378645752a4dc7107e91ec93e5aeaac00b9
Konrad Korus <konradkorus@gmail.com>
2017-07-23 12:56:51 +0100

    Added first test for SalarySlipGenerator.

--- SalarySlipKata/test/SalarySlip.Services.Tests/SalarySlipGeneratorShould.cs
+++ SalarySlipKata/test/SalarySlip.Services.Tests/SalarySlipGeneratorShould.cs
@@ -1,13 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
 
 namespace SalarySlip.Services.Tests
 {
     public class SalarySlipGeneratorShould
     {
-        
+        private readonly SalarySlipGenerator _salarySlipGenerator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _salarySlipGenerator = new SalarSlipGenerator();
+        }
+
+        [Test]
+        public void Return_SalarySlip_For_Employee()
+        {
+            // Arrange
+            Employee employee = new Employee();
+
+            // Act
+            SalarySlip salarySlip = _salarySlipGenerator.GenerateFor(employee);
+
+            // Assert
+            salarySlip.Id.Should().Be(12345);
+            salarySlip.Name.Should().Be("John J Doe");
+            salarySlip.GrossSalary.Should().Be(416.67M);
+        }
     }
 }
